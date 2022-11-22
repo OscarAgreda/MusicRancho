a2f9ac3547550d5a0ed7d855c57fac57a6b89f6b
HH <hans.herzl@live.com>
2022-11-22 16:59:16 -0600

    Code cleanup for Program class in Web project

--- MusicRancho_Web/Program.cs
+++ MusicRancho_Web/Program.cs
@@ -1,92 +1,107 @@
-using MusicRancho_Web;
-using MusicRancho_Web.Services.IServices;
-using MusicRancho_Web.Services;
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
-using System.Security.Claims;
+using MusicRancho_Web;
+using MusicRancho_Web.Services;
+using MusicRancho_Web.Services.IServices;
+
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
 builder.Services.AddAutoMapper(typeof(MappingConfig));
+
 builder.Services.AddHttpClient<IRanchoService, RanchoService>();
 builder.Services.AddScoped<IRanchoService, RanchoService>();
+
 builder.Services.AddHttpClient<IRanchoNumberService, RanchoNumberService>();
 builder.Services.AddScoped<IRanchoNumberService, RanchoNumberService>();
-builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
 builder.Services.AddHttpClient<IAuthService, AuthService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
+
 builder.Services.AddDistributedMemoryCache();
-builder.Services.AddAuthentication
-(options =>
-{
-    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = "oidc";
-})
-.AddCookie(options =>
-{
-    options.Cookie.HttpOnly = true;
-    options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
-    options.LoginPath = "/Auth/Login";
-    options.AccessDeniedPath = "/Auth/AccessDenied";
-    options.SlidingExpiration = true;
-})
-.AddOpenIdConnect("oidc", options =>
-{
-    options.Authority = builder.Configuration["ServiceUrls:IdentityAPI"];
-    options.GetClaimsFromUserInfoEndpoint = true;
-    options.ClientId = "music";
-    options.ClientSecret = "secret";
-    options.ResponseType = "code";
-    options.TokenValidationParameters.NameClaimType = "name";
-    options.TokenValidationParameters.RoleClaimType = "role";
-    options.Scope.Clear();
-    options.Scope.Add("music");
-    options.Scope.Add("openid");
-    options.Scope.Add("profile");
-    options.Scope.Add("email");
-    options.GetClaimsFromUserInfoEndpoint = true;
-    options.SaveTokens = true;
-    options.ClaimActions.MapJsonKey("role", "role");
-    options.ClaimActions.MapJsonKey("marca_de_carro", "marca_de_carro");
-    options.Events = new OpenIdConnectEvents
+
+builder
+    .Services
+    .AddAuthentication(options =>
     {
-        OnRedirectToIdentityProvider = context =>
+        options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = "oidc";
+    })
+    .AddCookie(options =>
+    {
+        options.Cookie.HttpOnly = true;
+        options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+        options.LoginPath = "/Auth/Login";
+        options.AccessDeniedPath = "/Auth/AccessDenied";
+        options.SlidingExpiration = true;
+    })
+    .AddOpenIdConnect("oidc", options =>
+    {
+        options.Authority = builder.Configuration["ServiceUrls:IdentityAPI"];
+        options.GetClaimsFromUserInfoEndpoint = true;
+        options.ClientId = "music";
+        options.ClientSecret = "secret";
+        options.ResponseType = "code";
+        options.TokenValidationParameters.NameClaimType = "name";
+        options.TokenValidationParameters.RoleClaimType = "role";
+        options.Scope.Clear();
+        options.Scope.Add("music");
+        options.Scope.Add("openid");
+        options.Scope.Add("profile");
+        options.Scope.Add("email");
+        options.GetClaimsFromUserInfoEndpoint = true;
+        options.SaveTokens = true;
+        options.ClaimActions.MapJsonKey("role", "role");
+        options.ClaimActions.MapJsonKey("marca_de_carro", "marca_de_carro");
+        
+        options.Events = new OpenIdConnectEvents
         {
-            context.ProtocolMessage.SetParameter("audience", "music");
-            return Task.CompletedTask;
-        },
-        OnRedirectToIdentityProviderForSignOut = context =>
-        {
-            context.ProtocolMessage.SetParameter("audience", "music");
-            return Task.CompletedTask;
-        },
-        OnRemoteFailure = context =>
-        {
-            context.Response.Redirect("/");
-            context.HandleResponse();
-            return Task.FromResult(0);
-        }
-    };
-});
+            OnRedirectToIdentityProvider = context =>
+            {
+                context.ProtocolMessage.SetParameter("audience", "music");
+                return Task.CompletedTask;
+            },
+            OnRedirectToIdentityProviderForSignOut = context =>
+            {
+                context.ProtocolMessage.SetParameter("audience", "music");
+                return Task.CompletedTask;
+            },
+            OnRemoteFailure = context =>
+            {
+                context.Response.Redirect("/");
+                context.HandleResponse();
+                return Task.FromResult(0);
+            }
+        };
+    })
+    ;
+
 builder.Services.AddSession(options =>
 {
     options.IdleTimeout = TimeSpan.FromMinutes(100);
     options.Cookie.HttpOnly = true;
     options.Cookie.IsEssential = true;
 });
+
 var app = builder.Build();
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 app.UseSession();
-app.MapControllerRoute(
-name: "default",
-pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
+
 app.Run();
