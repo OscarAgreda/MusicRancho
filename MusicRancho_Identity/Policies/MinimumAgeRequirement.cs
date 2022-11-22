06c572912cd2a7ee7dedef27652146d3bc94c88c
HH <hans.herzl@live.com>
2022-11-22 02:01:31 -0600

    Refactory for policies in Identity project

--- MusicRancho_Identity/Policies/MinimumAgeRequirement.cs
+++ MusicRancho_Identity/Policies/MinimumAgeRequirement.cs
@@ -1,13 +1,15 @@
 using Microsoft.AspNetCore.Authorization;
+
 namespace MusicRancho_Identity.Policies
 {
+    // ref: https://learn.microsoft.com/en-us/aspnet/core/security/authorization/policies?view=aspnetcore-6.0
     public class MinimumAgeRequirement : IAuthorizationRequirement
     {
-        //https://learn.microsoft.com/en-us/aspnet/core/security/authorization/policies?view=aspnetcore-6.0
         public MinimumAgeRequirement(int minimumAge)
         {
             MinimumAge = minimumAge;
         }
+
         public int MinimumAge { get; }
     }
 }
